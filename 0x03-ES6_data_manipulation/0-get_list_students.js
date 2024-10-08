18509c91ad67f7e0d53530d98d6106728773c864
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-10-08 21:52:09 +0000

    Modifiy the returned array to a array literal

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
-export default function getListStudents () {
-	const data1 = {id: 1, firstName: "Guillaume", location: "San Francisco"}
-	const data2 = {id: 2, firstName: "James", location: "Columbia"}
-	const data5 = {id: 5, firstName: "Serena", location: "San Francisco"}
-	return new Array(data1, data2, data5) 
+export default function getListStudents() {
+  const data1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const data2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const data5 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [data1, data2, data5];
 }
