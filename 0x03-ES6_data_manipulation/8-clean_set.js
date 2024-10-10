5bc75dd540e20258fc061548e6a4a3b9bf3f4966
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-10-10 00:26:44 +0000

    Fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString.length !== 0) {
+  if (typeof startString === 'string' && startString.length !== 0) {
     return [...set]
       .filter((value) => value.startsWith(startString))
       .map((value) => value.slice(startString.length))
