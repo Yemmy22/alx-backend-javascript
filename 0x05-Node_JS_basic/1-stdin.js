eca0f404ba5348bb93f99c9749b1ca067ce987ac
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-26 08:51:56 +0000

    Takes an input and prints a formated string with the input

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// Display initial message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Listen to standard input
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim(); // Get user input and trim extra spaces
+  console.log(`Your name is: ${name}`);
+  process.exit(); // Exit the process after displaying the name
+});
+
+// Listen for process exit and display closing message
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
