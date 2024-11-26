3d5705e575cb9ded12574c9bab098cc10361d594
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-26 22:20:57 +0000

    Fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,89 +1,70 @@
 const http = require('http');
-const fs = require('fs');
+const { readFile } = require('fs');
 
-// Function to count students, similar to 3-read_file_async.js
+const hostname = '127.0.0.1';
+const port = 1245;
+
 function countStudents(fileName) {
   const students = {};
   const fields = {};
   let length = 0;
-
   return new Promise((resolve, reject) => {
-    fs.readFile(fileName, 'utf-8', (err, content) => {
+    readFile(fileName, (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = content.toString().split('\n');
-      for (let i = 0; i < lines.length; i += 1) {
-        if (lines[i]) {
-          length += 1;
-          const field = lines[i].toString().split(',');
-          const studentName = field[0];
-          const studentField = field[3];
-
-          if (students[studentField]) {
-            students[studentField].push(studentName);
-          } else {
-            students[studentField] = [studentName];
-          }
-
-          if (fields[studentField]) {
-            fields[studentField] += 1;
-          } else {
-            fields[studentField] = 1;
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
           }
         }
-      }
-
-      const totalStudents = length - 1; // excluding the header
-      let output = `Number of students: ${totalStudents}\n`;
-
-      for (const [field, count] of Object.entries(fields)) {
-        if (field !== 'field') {
-          output += `Number of students in ${field}: ${count}. List: ${students[field].join(', ')}\n`;
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
         }
+        resolve(output);
       }
-      resolve(output.slice(0, -1)); // Remove the last newline
     });
   });
 }
 
-// Create HTTP server
 const app = http.createServer((req, res) => {
-  const { url } = req;
-  const { method } = req;
-
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  if (method === 'GET') {
-    if (url === '/') {
-      res.write('Hello Holberton School!');
-      res.end();
-    } else if (url === '/students') {
-      const fileName = process.argv[2]; // Get the filename from the command-line arguments
-      countStudents(fileName)
-        .then((output) => {
-          res.write('This is the list of our students\n');
-          res.end(output); // end with the formatted output without extra newline
-        })
-        .catch(() => {
-          res.statusCode = 404;
-          res.end('Cannot load the database');
-        });
-    } else {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    }).catch(() => {
       res.statusCode = 404;
-      res.end('Not Found');
-    }
-  } else {
-    res.statusCode = 405;
-    res.end('Method Not Allowed');
+      res.end('Cannot load the database');
+    });
   }
 });
 
-// Listen on port 1245
-app.listen(1245, '127.0.0.1', () => {
+app.listen(port, hostname, () => {
 });
 
 module.exports = app;
