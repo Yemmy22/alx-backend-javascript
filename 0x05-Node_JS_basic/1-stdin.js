0f39ffe4635dec28c31c17170a96f9fb97e6fef7
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-26 14:36:41 +0000

    Fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,21 @@
 // Display initial message.
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen to standard input.
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // Get user input and trim extra spaces
-  console.log(`Your name is: ${name}`);
-  process.exit(); // Exit the process after displaying the name
-});
-
-// Listen for process exit and display closing message.
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+if (process.stdin.isTTY) {
+  // Listen to standard input.
+  process.stdin.on('data', (data) => {
+    // Get user input and trim extra spaces
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    // Exit the process after displaying the name
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  // Listen for process exit and display closing message.
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
