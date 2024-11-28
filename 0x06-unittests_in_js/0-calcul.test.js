5a28106fcce37289fcf8346d4301f7ade121df60
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-28 05:58:17 +0000

    Fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,22 +3,22 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should return 4 when adding 1 and 3', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3), 4);
   });
 
   it('should return 5 when adding 1 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
 
   it('should return 5 when adding 1.2 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
 
   it('should return 6 when adding 1.5 and 3.7', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 
   it('should handle negative numbers correctly', () => {
-    assert.strictEqual(calculateNumber(-1.5, -3.2), -4);
+    assert.equal(calculateNumber(-1.5, -3.2), -4);
   });
 });
